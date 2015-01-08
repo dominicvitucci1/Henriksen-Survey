7ef4406fee3e20e10a57888ed3aaf6a9cdac3974
dominicvitucci1 <Dominic@host-20-166.ilcuito.clients.pavlovmedia.com>
2015-01-08 16:13:39 -0600

    Initial Commit

---
+++ Henriksen Survey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Henriksen Survey
+//
+//  Created by Dominic Vitucci on 1/8/15.
+//  Copyright (c) 2015 Dominic Vitucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
